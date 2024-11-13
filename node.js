6d643e7f337f6f213f698be4f69d6ad22aaffa2a
unicornfarm5 <linea.moltved@gmail.com>
2024-11-13 11:17:02 +0100

    davs

--- node.js
+++ node.js
@@ -55,9 +55,9 @@ app.get('/all', (req, res) => {
 //er ikke sikker på om jeg har joinet rigtigt // om jeg ahr husket navnene rigtigt
 app.get('/adress/:city', (req, res) => {
     const UserRequest = req.params.adress
-    connection.query('SELECT `name` FROM bar
-    join adress on bar.bar_id = bar_adress.bar_id
-    WHERE bar_adress.adress =' 'UserRequest' ,
+    connection.query('SELECT `name` FROM bar ' +
+        'join adress on bar.bar_id = bar_adress.bar_id' +
+        'WHERE bar_adress.adress =' + 'UserRequest',
     (error, results) => {
         res.send(results);
     });
