e8710437445be596632535477c0a4a32052d8198
unicornfarm5 <linea.moltved@gmail.com>
2024-11-13 11:02:41 +0100

    updated main

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ app.get('/all', (req, res) => {
         res.send(results);
     })
 })
+
+//finder barer ud fra bynavn
+//er ikke sikker på om jeg har joinet rigtigt // om jeg ahr husket navnene rigtigt
+app.get('/adress/:city', (req, res) => {
+    const UserRequest = req.params.adress
+    connection.query('SELECT `name` FROM bar
+        join adress on bar.bar_id = bar_adress.bar_id 
+        WHERE bar_adress.adress =' 'UserRequest' ,
+        (error, results) => {
+            res.send(results);
+        });
+});
