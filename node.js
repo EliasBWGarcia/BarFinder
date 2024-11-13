b21f25f9e34abf9c7df5d693f2e18c74dfaa93b7
eliasbwgarcia <eliasbenjamin@live.dk>
2024-11-13 11:15:59 +0100

    added connnection to MYSQL, connected to myDB, added express,

--- node.js
+++ node.js
@@ -1,3 +1,4 @@
+//npm install express --save
 const express = require("express");
 //npm install mysql2 --save
 const mysql = require("mysql2");
@@ -11,23 +12,23 @@ app.use(cors());
 
 //Host, user, password database
 const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"hejsa",
-    database:"pokemon"
+    host:process.env.host,
+    user:process.env.user,
+    password: process.env.password,
+    database: process.env.database
 });
 
 app.get('/',(req, res)=>{
-    connection.query('SELECT `name` FROM pokemon',(error,results)=>{
+    connection.query('SELECT `name` FROM mydb',(error,results)=>{
         res.send(results);
     });
 });
 
 //Find single pokemon by name
-app.get('/pokemon/:name/', (req,res)=>{
-    const pokemonUserRequest = req.params.name;
-    connection.query('SELECT * FROM pokemon WHERE ´name´ = ?',
-        [pokemonUserRequest],
+app.get('/bar/:bar.name/', (req,res)=>{
+    const barNameRequest = req.params.name;
+    connection.query('SELECT * FROM mydb WHERE ´name´ = ?',
+        [barNameRequest],
         (error, results)=>{
             res.send(results);
         });
